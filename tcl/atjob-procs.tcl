67e959e23ef6bdddf251d4e3bc1f4edff51a5420
Gustaf Neumann <neumann@wu-wien.ac.at>
2014-03-03 19:29:21 +0100

    - some cleanup (many thanks to thomas renner)

--- tcl/atjob-procs.tcl
+++ tcl/atjob-procs.tcl
@@ -6,17 +6,6 @@ namespace eval ::xowf {
   #
   # define a simple acs-type for at jobs
   #
-#   ::xo::db::Class create ::xowf::atjob -superclass ::xo::db::Object  -slots {
-#     ::xo::db::Attribute create owner_id -datatype integer -references acs_objects(object_id)
-#     ::xo::db::Attribute create party_id -datatype integer -references parties(party_id)
-#     ::xo::db::Attribute create cmd
-#     ::xo::db::Attribute create done -datatype boolean -default f
-#     ::xo::db::Attribute create time -datatype timestamp
-#     ::xo::Attribute create object
-#   }
-#   ::xo::db::require index -table xowf_atjob -col done
-#   ::xo::db::require index -table xowf_atjob -col time
-
   Class create ::xowf::atjob -slots {
     ::xo::Attribute create owner_id
     ::xo::Attribute create party_id
@@ -37,71 +26,6 @@ namespace eval ::xowf {
   atjob instproc init {} {
     my destroy_on_cleanup
   }
-
-#   atjob instproc persist {} {
-#     my instvar party_id cmd
-
-#     set class [self class]
-#     set owner_id [[my object] item_id]
-#     set package_id [[my object] package_id]
-#     set sql_timestamp [$class sql_timestamp [clock scan [my time]]]
-#     if {![info exists party_id]} {my party_id [::xo::cc set untrusted_user_id]}
-
-#     set already_recorded [db_0or1row [my qn is_recorded] "
-#       select 1 from [$class table_name]
-#       where owner_id = :owner_id and party_id = :party_id and cmd = :cmd 
-#       and time = $sql_timestamp"]
-
-#     if {$already_recorded} {
-#       my debug_message "entry already recorded"
-#     } else {
-#       $class get_context package_id creation_user creation_ip
-#       db_transaction {
-#         set id [$class new_acs_object \
-#                     -package_id $package_id \
-#                     -creation_user $creation_user \
-#                     -creation_ip $creation_ip \
-#                     ""]
-#         $class initialize_acs_object [self] $id
-#         db_dml [my qn insert_job] \
-#             "insert into [$class table_name] ([$class id_column], owner_id, time, party_id, cmd) \
-#              values ($id, $owner_id, $sql_timestamp, $party_id, :cmd)"
-#       }
-#     }
-#   }
-
-
-#   atjob proc check {{-with_older false}} {
-#     #
-#     # check, if there is something to do
-#     #
-#     set op [expr {$with_older ? "<=" : "=" }]
-#     set items [my get_instances_from_db \
-#                    -where_clause "and done='f' and time $op [my sql_timestamp [clock seconds]]"]
-    
-#     foreach job [$items children] {
-#       my log "\n*** job=[$job serialize] ***\n"
-#       set owner_id [$job owner_id]
-
-#       # We assume, the owner object is a cr-item
-#       ::xo::db::CrClass get_instance_from_db -item_id $owner_id
-
-#       # We assume, the package is from the xowiki family; make sure, the url looks like real
-#       ::xo::Package initialize \
-#           -package_id [$owner_id package_id] \
-#           -user_id [$job party_id] \
-#           -init_url 0 -actual_query ""
-#       $package_id set_url -url [$package_id package_url][$owner_id name]
-
-#       my log "\n*** executing atjob [$job cmd] ***\n"
-#       if {[catch {eval [$job cmd]} errorMsg]} {
-#         ns_log error "\n*** atjob [$job cmd] lead to error ***\n$errorMsg"
-#       } else {
-#         $job done t
-#         $job save
-#       }
-#     }
-#   }
 
   #
   # 
@@ -160,11 +84,10 @@ namespace eval ::xowf {
 
     set items [::xowiki::FormPage instantiate_objects \
 		   -object_class ::xowiki::FormPage \
-		   -sql "
-      			select i.item_id, i.name, i.parent_id, i.publish_status, o.creation_user, 
-                             r.revision_id, page_template, instance_attributes 
+		   -sql "select i.item_id, i.name, i.parent_id, i.publish_status, o.creation_user,
+                             i.live_revision as revision_id, page_template, instance_attributes
                       	from cr_items i, xowiki_page_instance t, acs_objects o
-			where i.item_id in ([join $items_ids ,]) and 
+			where i.item_id in ([join $item_ids ,]) and
 			i.live_revision = t.page_instance_id and o.object_id = i.item_id"]
 
     my log "--at we got [llength [$items children]] scheduled items"
@@ -221,12 +144,12 @@ namespace eval ::xowf {
     # To make sure we are not fetching pages from unmounted instances
     # we check for package_id not null.
     #
-    set item_ids [::xo::dc list get_due_atjobs "
+    set item_ids ::xo::dc list get_due_atjobs "
                       select i.item_id
                       from cr_items i, cr_items i2, cr_revisions r, xowiki_page_instance t, acs_objects o
                       where i.item_id = r.item_id and i.live_revision = r.revision_id 
                       and r.revision_id = t.page_instance_id and o.object_id = i.item_id
-                      and i2.item_id = i.parent_id and i2.content_type = '::xowiki::FormPage'
+                      and i2.item_id = t.page_template and i2.content_type = '::xowiki::Form'
                       and i2.name = 'en:atjob-form'
                       and r.publish_date $op to_timestamp(:ansi_time,'YYYY-MM-DD HH24:MI')
                       and i.publish_status = 'production'
@@ -243,11 +166,13 @@ namespace eval ::xowf {
     # waiting. If the list of jobs gets large, we might consider
     # splitting the list and run multiple jobs in parallel.
     #
-    set queue xowfatjobs
-    if {$queue ni [ns_job queues]} {
-        ns_job create $queue
+    if {[llength $item_ids]} {
+        set queue xowfatjobs
+        if {$queue ni [ns_job queues]} {
+            ns_job create $queue
+        }
+        ns_job queue -detached $queue [list ::xowf::atjob run_jobs $item_ids]
     }
-    ns_job queue -detached $queue [list ::xowf::atjob run_jobs $item_ids]
 
     my log "--at END"
   }
